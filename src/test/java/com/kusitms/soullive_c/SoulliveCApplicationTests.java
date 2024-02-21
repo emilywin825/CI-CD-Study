3768a155e60ad1eab742df90e6e3438db564bcc6
emilywin825 <emilywin825@naver.com>
2024-02-22 03:39:24 +0900

    스프링 파일 업로드

---
+++ src/test/java/com/kusitms/soullive_c/SoulliveCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusitms.soullive_c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulliveCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
