c7e99dacc8a6ee6a7407b4cd18b6de4ac473f6e3
emilywin825 <emilywin825@naver.com>
2024-02-22 03:16:42 +0900

    스프링 파일 업로드

---
+++ src/main/java/com/kusitms/soullive_c/SoulliveCApplication.java
@@ -0,0 +1,13 @@
+package com.kusitms.soullive_c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulliveCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulliveCApplication.class, args);
+	}
+
+}
